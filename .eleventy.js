e63fc0b6331eb30210ba502becbac41c1844ae1d
ngtrend <srisubaramng@gmail.com>
2025-12-19 07:45:50 +0530

    Switch to dynamic sitemap generation with Eleventy plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,19 @@
 module.exports = function(eleventyConfig) {
   // Copy static assets
   eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPassthroughCopy("sitemap.xml");
   eleventyConfig.addPassthroughCopy("robots.txt");
+
+  // Sitemap plugin
+  const sitemap = require("@quasibit/eleventy-plugin-sitemap");
+  eleventyConfig.addPlugin(sitemap, {
+    sitemap: {
+      hostname: "https://sinztools.app",
+      lastModifiedProperty: "modified",
+      sitemapFunction: (collection) => {
+        return collection.getAll().filter(item => item.url && !item.url.includes('/tags/'));
+      }
+    }
+  });
 
   return {
     dir: {
